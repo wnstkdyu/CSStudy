c8c552e4614f92725ca8eb0f0c54735d5b46e4c6
alpaca <wnstkdyu@gmail.com>
2018-03-07 02:51:56 +0900

    합병 정렬, 퀵 정렬 추가

---
+++ Algorithm/Sort/MergeSort.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+/*
+ * MergeSort
+ * Divide & Conquer 전략을 이용해 정렬하는 방법으로 재귀적 방법을 사용한다.
+ * 이전의 기본 정렬과 다르게 O(nlogn)의 빠른 시간 복잡도를 보인다.
+ * 이는 평균적으로나 최악이나 같아 안정적이라 할 수 있다.
+ * 대신 임의의 빈 배열을 만들어 사용하여 O(n)의 공간 복잡도를 보인다.
+ * 그러나 연결 리스트에서는 O(1)만 필요해 연결 리스트의 정렬에 적합하다.
+ */
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] testArr = {3, 5, 1, 9, 2};
+		int[] sortedArr = merge_sort(testArr, 0, testArr.length - 1);
+		
+		for (int num: sortedArr) {
+			System.out.print(num);
+		}
+	}
+	
+	public static int[] merge_sort(int[] arr, int p, int r) {
+		if (p < r) {
+			int q = (p + r) / 2;
+			
+			merge_sort(arr, p, q);
+			merge_sort(arr, q + 1, r);
+			merge(arr, p, q, r);
+		}
+		
+		return arr;
+	}
+	
+	public static void merge(int[] arr, int p, int q, int r) {
+		// 정렬된 arr[p...q]와 arr[q+1...r]을 합쳐
+		// 정렬된 arr[p...r]을 만든다.
+		int i = p;
+		int j = q + 1;
+		
+		int[] tempArr = new int[arr.length];
+		System.out.println(tempArr.length);
+		
+		int k = p;
+		
+		while (i <= q && j <= r) {
+			if (arr[i] <= arr[j]) {
+				tempArr[k] = arr[i];
+				i++;
+			} else {
+				tempArr[k] = arr[j];
+				j++;
+			}
+			
+			k++;
+		}
+		
+		// 남아 있는 것 처리
+		// 1) 왼쪽이 남아 있으면 
+		while (i <= q) {
+			tempArr[k] = arr[i];
+			i++;
+			k++;
+		} 
+		// 2) 오른쪽이 남아 있으면 
+		while (j <= r) {
+			tempArr[k] = arr[j];
+			j++;
+			k++;
+		}
+		
+		for (int h = p; h <= r; h++) {
+			arr[h] = tempArr[h];
+		}
+	}
+}
---
+++ Algorithm/Sort/QuickSort.java
@@ -0,0 +1,57 @@
+/*
+ * QuickSort
+ * Pivot을 두어 그것을 기준으로 
+ * 왼쪽은 피벗의 값보다 작은 배열,
+ * 오른쪽은 피벗의 값보다 큰 배열로 나누어 다시 정렬을 한다.
+ * 따라서 Divide&Conquer 방식으로 재귀를 사용한다.
+ * 
+ * 최악의 경우(이미 정렬된 경우) O(n^2)의 시간복잡도를 보이지만,
+ * 평균, 최상의 경우 O(nlogn)의 시간복잡도를 보인다.
+ * 그리고 보통의 경우에 빠른 정렬 방법이다.
+ */
+public class QuickSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] testArr = {3, 5, 1, 9, 2};
+		int[] sortedArr = quick_sort(testArr, 0, testArr.length - 1);
+		
+		for (int num: sortedArr) {
+			System.out.print(num);
+		}
+	}
+	
+	public static int[] quick_sort(int[] arr, int p, int r) {
+		if (p < r) {
+			int q = partition(arr, p, r);
+			quick_sort(arr, p, q - 1);
+			quick_sort(arr, q + 1, r);
+		}
+		
+		return arr;
+	}
+	
+	public static int partition(int[] arr, int p, int r) {
+		// 배열 arr[p...r]의 원소들을 arr[r], 즉 pivot을 기준으로 양쪽으로 재배치하고
+		// A[r]이 위치한 자리를 반환한다.
+		int pivot = arr[r];
+		int lastLowerIndex = p - 1;
+		
+		for (int currentCheckIndex = p; currentCheckIndex < r; currentCheckIndex++) {
+			if (arr[currentCheckIndex] < pivot) {
+				lastLowerIndex++;
+				
+				// arr[lastLowerIndex]와 arr[currentCheckIndex]를 바꿔준다.
+				int temp = arr[lastLowerIndex];
+				arr[lastLowerIndex] = arr[currentCheckIndex];
+				arr[currentCheckIndex] = temp;
+			}
+		}
+		// pivot의 위치를 잡아준다.
+		int temp = arr[lastLowerIndex + 1];
+		arr[lastLowerIndex + 1] = pivot;
+		arr[r] = temp;
+		
+		return lastLowerIndex + 1;
+	}
+}
