dc5deeb96088eb6340deb8aab6c8c1758935897c
alpaca <wnstkdyu@gmail.com>
2018-06-11 16:22:00 +0900

    퀵소트 코드 띄어쓰기 및 주석 수정

--- Algorithm/Sort/QuickSort.swift
+++ Algorithm/Sort/QuickSort.swift
@@ -2,13 +2,15 @@
 // 분할 정복이지만 병합 과정이 없음. 분할 과정에서 이미 넣어지기 때문.
 // 멋진 코드여서 가져와서 수정했다.
 // 출처: http://minsone.github.io/programming/quick-sort-in-swift
+
 func quickSort(array: [Int]) -> [Int] {
     guard !array.isEmpty else { return [] }
     let pivot = array[0]
     
-    return quickSort(array: array.filter{ $0 <= pivot }) + [pivot] + quickSort(array: array.filter{ $0 > pivot })
+    return quickSort(array: array.filter { $0 <= pivot }) + [pivot] + quickSort(array: array.filter { $0 > pivot })
 }
 
+// 제네릭 버전
 func quickSort<T: Comparable>(array: [T]) -> [T] {
     guard !array.isEmpty else { return [] }
     let pivot = array[0]
